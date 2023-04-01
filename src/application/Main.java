326e2db781ac91140f023edd01a14dd809226853
Kate Dinh <kqtednh@gmail.com>
2023-03-31 20:08:24 -0700

    testing pushing

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,12 +6,25 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
 public class Main extends Application {
+	private static Stage stg; 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			stg = primaryStage;
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("LogIn.fxml"));
+			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -19,7 +32,10 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+	 public void changeScene(String fxml) throws IOException {
+	        BorderPane pane = FXMLLoader.load(getClass().getResource(fxml));
+	        stg.getScene().setRoot(pane);
+	    }
 	public static void main(String[] args) {
 		launch(args);
 	}
